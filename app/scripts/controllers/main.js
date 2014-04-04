016d568c93f1ba61e1529ec7b092152fb1bd8518
Shekhar Gulati <shekhargulati84@gmail.com>
2014-04-05 04:19:54 +0530

    view story working with $http backend

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,12 +2,6 @@
 
 angular.module('hackerpins')
     .controller('MainCtrl', function ($scope, $http) {
-        $scope.stories = [
-            {id: 1, "title": "OpenShift Rocks", submittedAt: Date.now()},
-            {id: 2, "title": "OpenShift and Java EE 7 Rocks", submittedAt: Date.now()},
-            {id: 3, "title": "OpenShift + Java EE 7 + AngularJS Rocks", submittedAt: Date.now()}
-        ];
-
         $scope.delete = function (story, index) {
             alert("Delete called for " + index);
             alert("Story " + JSON.stringify(story));
--- app/scripts/controllers/viewstory.js
+++ app/scripts/controllers/viewstory.js
@@ -1,11 +1,13 @@
 'use strict';
 
 angular.module('hackerpins')
-    .controller('ViewstoryCtrl', function ($scope) {
-        $scope.story = {
-            id: 1,
-            title: "OpenShift Rocks++",
-            description: "OpenShift is an awesome platform to host Java applications",
-            submittedAt: Date.now()
-        }
+    .controller('ViewstoryCtrl', function ($scope, $http, $routeParams, $location) {
+
+        var storyId = $routeParams.id;
+        $http.get('http://localhost:8080/hackerpins/api/v1/stories/' + storyId).success(function (data, status, headers, config) {
+            $scope.story = data;
+        }).error(function (data, status, headers, config) {
+            $location.path('/');
+        });
+
     });
